a61901d0deca8f980dc05729d8fbadea33b8c558
Jacklau9515 <Jacklau9515@gmail.com>
2018-03-07 02:16:45 +1100

    Initial commit

---
+++ app/components/Log.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import { clearInterval } from 'timers';
+
+const Switch = ({value, isPaused}) => (
+    <div>
+        <button className='btn' onClick={isPaused}>{value}</button>
+    </div>
+);
+
+/**const reducer = (state = 'Pause', action) => {
+    switch (action.type) {
+        case 'ISPAUSED':
+            return state = 'Resume';  
+        case 'UNPAUSED':
+            return state;
+        default:
+            return state;
+    };
+};**/
+
+/**const store = createStore(reducer);**/
+
+/**const render = () => {
+    render(<button
+            value={store.getState()}
+            isPaused={() => store.dispatch({type:'ISPAUSED'})}
+            unPaused={() => store.dispatch({type:'UNPAUSED'})}
+            />,
+        document.getElementById('btn')
+    );
+};
+
+store.subscribe(render);**/
+
+class Log extends Component {
+  constructor(){
+    super();
+    this.state={
+      data:[],
+      time: new Date(),
+      isPaused: false
+    };
+  }
+
+  handleClickOnLikeButton () {
+    this.setState({
+      isPaused: !this.state.isPaused
+    })
+  }
+
+  fetchJSON(){
+    fetch('https://join.reckon.com/stock-pricing', {
+        method: 'GET'
+      }).then((Response) => Response.json())
+      .then((jsonData) => {
+        console.log(jsonData)
+        this.setState({
+          data:jsonData
+        });
+      });
+  }
+
+  currentTime(){
+    this.setState({
+      time: new Date()
+    })
+  }
+
+  myStopFunction() {
+      clearInterval(this.timer);
+    }
+
+  componentDidMount(){
+    this.timer = setInterval(()=> this.fetchJSON(), 2000),
+    this.datetime = setInterval(()=>this.currentTime(), 2000)
+  }
+
+  render () {
+    const pausedLog = this.props.likedText || 'Resume' 
+    const resumeLog = this.props.unlikedText || 'Pause'
+    return (
+      <div className='logcavans'>
+        <div className='logtitle'><h1>Log</h1></div>
+        <button className='btn' onClick={this.handleClickOnLikeButton.bind(this)}>
+        {this.state.isPaused ? pausedLog : resumeLog} Log
+        </button>
+        <div className='log'>
+        <span>Updates for {this.state.time.toLocaleDateString()}{this.state.time.toLocaleTimeString()}</span>
+        {
+          this.state.data.map((dynamicData,key)=>
+          <div>
+            <span>{dynamicData.code}  :  </span>
+            <span> ${dynamicData.price}</span>
+          </div>
+          )
+        }
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default Log;
---
+++ app/components/Summary.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { setInterval } from 'timers';
+
+class Summary extends Component {
+    constructor(){
+        super();
+        this.state={
+          data:[],
+          predata:[],
+          lastdata:[]
+        };
+      }
+    
+      fetchJSON(){
+        fetch('https://join.reckon.com/stock-pricing', {
+            method: 'GET'
+          }).then((Response) => Response.json())
+          .then((jsonData) => {
+            console.log(jsonData)
+            this.setState({
+              data:jsonData
+            });
+          });
+      }
+
+      fetchlastJSON(){
+        fetch('https://join.reckon.com/stock-pricing', {
+            method: 'GET'
+          }).then((Response) => Response.json())
+          .then((jsonData) => {
+            console.log(jsonData)
+            this.setState({
+              lastdata:jsonData
+            });
+          });
+      }
+
+      fetchpreJSON(){
+        fetch('https://join.reckon.com/stock-pricing', {
+            method: 'GET'
+          }).then((Response) => Response.json())
+          .then((jsonData) => {
+            console.log(jsonData)
+            this.setState({
+              predata:jsonData
+            });
+          });
+      }
+    
+      componentDidMount(){
+        this.timer = setInterval(()=> this.fetchJSON(), 2000),
+        this.lasttimer = setInterval(()=> this.fetchlastJSON(), 4000),
+        this.fetchpreJSON()
+      }
+    
+
+  render () {
+    return (
+        <div className='sumcavans'>
+            <div className='sumtitle'><h1>Summary</h1></div>
+            <table className='pricetable' border="1">
+              <tr>
+                  <td>Stock</td><br/><br/>
+                  <td>Starting</td><br/><br/>
+                  <td>Lowest</td><br/><br/>
+                  <td>Highest</td><br/><br/>
+                  <td>Current</td><br/><br/>
+              </tr>
+              {
+                  this.state.data.map((dynamicData,key)=>
+              <tr>
+                  <td>{dynamicData.code}</td><br/><br/>
+                  <td>{this.state.predata[key].price}</td><br/><br/>
+                  <td>{dynamicData.price<this.state.predata[key].price ? dynamicData.price : this.state.predata[key].price}</td><br/><br/>
+                  <td>{dynamicData.price>this.state.predata[key].price ? dynamicData.price : this.state.predata[key].price}</td><br/><br/>
+                  <td>{dynamicData.price}</td><br/><br/>
+             </tr>
+             )
+             }
+             </table>
+        </div>
+        );
+    }
+}
+
+export default Summary;
